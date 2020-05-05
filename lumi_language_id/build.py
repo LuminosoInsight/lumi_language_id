12007c3dd3aa582a2a553fa92c9c4543063a8bb3
Robyn Speer <rspeer@luminoso.com>
2020-05-05 15:54:17 -0400

    add a (messy) script for re-tuning the classifier

---
+++ lumi_language_id/build.py
@@ -0,0 +1,73 @@
+import itertools
+import langcodes
+import numpy as np
+
+from lumi_language_id import LanguageIdentifier
+from lumi_language_id.data_sources import twitter_gen, wiki_gen, tatoeba_gen
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score, log_loss
+
+
+def get_training_data():
+    return make_input_and_output(
+        itertools.chain(twitter_gen(), wiki_gen())
+    )
+
+
+def get_test_data():
+    return make_input_and_output(
+        tatoeba_gen()
+    )
+
+
+def make_input_and_output(input_gen):
+    inputs = []
+    outputs = []
+    lid = LanguageIdentifier()
+    for text, label in input_gen:
+        row, detected_lang = lid.make_data_point(text)
+        match = (langcodes.tag_distance(label, detected_lang) <= 5)
+        inputs.append(row)
+        outputs.append(match)
+    return np.array(inputs), np.array(outputs)
+
+
+def make_estimator():
+    return MLPClassifier(activation='relu', hidden_layer_sizes=(3, 3), alpha=0.1, max_iter=1000)
+    # return SVC(kernel='linear', probability=True)
+
+def run():
+    estimator = make_estimator()
+    input_train, output_train = get_training_data()
+    input_train, input_val, output_train, output_val = train_test_split(input_train, output_train, test_size=0.2)
+    estimator.fit(input_train, output_train)
+    input_test, output_test = get_test_data()
+
+
+    orig_confidence = input_val[:, 1]
+    weights_val = np.ones(output_val.shape) * output_val + np.ones(output_val.shape)
+    # orig_output = np.array([True] * len(orig_confidence))
+    orig_loss = log_loss(output_val, orig_confidence, labels=[False,True], sample_weight=weights_val)
+    print(f'Original log loss: {orig_loss:3.3f}')
+
+    predictions = estimator.predict(input_val)
+    model_accuracy = balanced_accuracy_score(predictions, output_val)
+    
+    predictions_p = estimator.predict_proba(input_val)[:,1]
+    model_loss = log_loss(output_val, predictions_p, sample_weight=weights_val)
+    print(f'Validation accuracy: {model_accuracy:3.3f}')
+    print(f'Log loss: {model_loss:3.3f}')
+
+    predictions = estimator.predict(input_test)
+    model_accuracy = balanced_accuracy_score(predictions, output_test)
+    predictions_p = estimator.predict_proba(input_test)[:,1]
+    model_loss = log_loss(output_test, predictions_p)
+    print(f'Test accuracy: {model_accuracy:3.3f}')
+    print(f'Log loss: {model_loss:3.3f}')
+
+    return estimator
+
+
+if __name__ == '__main__':
+    run()
---
+++ lumi_language_id/data_sources.py
@@ -0,0 +1,46 @@
+import csv
+import fasttext
+import pycld2 as cld2
+import langcodes
+from tabulate import tabulate
+import ftfy
+
+from lumi_language_id import data_file, align_language_to_fasttext
+
+
+def twitter_gen():
+    with open(data_file('TweetLID_corpusV2/tweetlid-test-tweets.tsv')) as twitter_file:
+        reader = csv.reader(twitter_file, delimiter='\t')
+        for row in reader:
+            # If there are multiple possibilities separated by +, take the first one
+            label = row[2].split('+')[0]
+            text = ''.join(x for x in row[3] if x.isprintable()).replace('\n', ' ')
+            text = ftfy.fix_text(text)
+            fixed_label = align_language_to_fasttext(label)
+            if fixed_label != 'und':
+                yield (text, fixed_label)
+
+
+def wiki_gen():
+    with open(data_file('WiLI/x_test.txt')) as texts_file:
+        with open(data_file('WiLI/y_test.txt')) as labels_file:
+            for text, label in zip(texts_file, labels_file):
+                label = label.rstrip()
+                text = ''.join(x for x in text if x.isprintable()).replace('\n', ' ')
+                text = ftfy.fix_text(text)
+                fixed_label = align_language_to_fasttext(label)
+                if fixed_label != 'und':
+                    yield (text, fixed_label)
+
+
+def tatoeba_gen():
+    with open(data_file('Tatoeba/tatoeba_short_text.txt')) as tatoeba_file:
+        for line in tatoeba_file:
+            line = line.split('\t')
+            text = line[1].replace('\n', ' ')
+            text = ftfy.fix_text(text)
+            label = line[0]
+            fixed_label = align_language_to_fasttext(label)
+            if fixed_label != 'und':
+                yield (text, fixed_label)
+
