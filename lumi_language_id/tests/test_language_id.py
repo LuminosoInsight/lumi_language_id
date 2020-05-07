63cc2137e0bf47bc11509ed212152006a2f7a4f1
Robyn Speer <rspeer@luminoso.com>
2020-05-07 16:42:32 -0400

    add simple test cases

---
+++ lumi_language_id/tests/test_language_id.py
@@ -0,0 +1,16 @@
+from lumi_language_id.tuned import TunedLanguageIdentifier
+
+
+LID = TunedLanguageIdentifier.load()
+
+
+def _get_language(text):
+    lang, prob = LID.detect_language(text)
+    return lang
+
+
+def test_language_id():
+    assert _get_language("here's some words") == 'en'
+    assert _get_language("aquí hay algunas palabras") == 'es'
+    assert _get_language("これらは言葉です") == 'ja'
+    assert _get_language("这些是单词") == 'zh'
