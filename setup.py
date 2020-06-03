cef5c2582ef4887f54902b10264b7940cc4cf49b
Robyn Speer <rspeer@luminoso.com>
2020-06-03 17:59:02 -0400

    include data in the sdist

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name="lumi_language_id",
-    version='0.2',
+    version='0.2.1',
     maintainer='Robyn Speer',
     maintainer_email='rspeer@luminoso.com',
     description='For when you want fastText language identification, but you also want to believe the answers',
     license='MIT',
     platforms=["any"],
     packages=['lumi_language_id'],
+    package_data={'lumi_language_id': ['data/*.npz', 'data/*.ftz']},
     install_requires=['fasttext', 'numpy', 'ftfy', 'langcodes >= 2'],
 
     # Training the tuned model requires scikit-learn, but using the model doesn't.
